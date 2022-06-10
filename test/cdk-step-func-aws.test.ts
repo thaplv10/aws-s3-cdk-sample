58e8628b4e1cd7913db415a4ee01b645aa0d6a63
ThapLV <quangthap9x@gmail.com>
2022-06-10 14:44:32 +0700

    Initial commit

---
+++ test/cdk-step-func-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStepFuncAws from '../lib/cdk-step-func-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-step-func-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStepFuncAws.CdkStepFuncAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
